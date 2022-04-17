dd5f0d7ba2c814d83312061f7110cb399f4449e4
Duvan Molina <duvanmoliina@gmail.com>
2022-04-17 18:49:42 -0500

    fix(web): comment

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ import Document, {
   DocumentContext,
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
-
+// @ts-ignore
 export default class CustomDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
